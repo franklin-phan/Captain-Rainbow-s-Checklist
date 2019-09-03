58d52949e6871378298d5b9eeebd344cba518dca
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2019-09-02 21:29:58 -0700

    Rework of step3

--- checklist.py
+++ checklist.py
@@ -14,26 +14,22 @@ def update(index, item):
 def destroy(index):
     checklist.pop(index)
 
-#Testing Code
-def test():
-    create("purple sox")
-    create("red cloak")
-
-    print(read(0))
-    print(read(1))
-
-    update(0, "purple socks")
-
-    destroy(1)
-
-    print(read(0))
-
-    list_all_items()
-
 def list_all_items():
     index = 0
     for list_item in checklist:
         print("{} {}".format(index, list_item))
         index += 1
-#def mark_completed(index):
+
+def test():
+        create("purple sox")
+        create("red cloak")
+
+        print(read(0))
+        print(read(1))
+
+        update(0, "purple socks")
+        destroy(1)
+
+        print(read(0))
+        print(read(1))
 test()
